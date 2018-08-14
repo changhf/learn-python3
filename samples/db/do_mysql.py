b5ea7cfb8993d2b5df0afbe48c15de4d08f5040b
常华锋 <wb-chf309549@alibaba-inc.com>
2018-08-14 16:09:15 +0800

    modify

--- samples/db/do_mysql.py
+++ samples/db/do_mysql.py
@@ -9,13 +9,14 @@
 import mysql.connector
 
 # change root password to yours:
-conn = mysql.connector.connect(user='root', password='password', database='test')
+conn = mysql.connector.connect(host='localhost',port=3306,user='root', password='root', database='mydemo',charset='utf8')
 
 cursor = conn.cursor()
+cursor.execute('drop table IF EXISTS user')
 # 创建user表:
 cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
 # 插入一行记录，注意MySQL的占位符是%s:
-cursor.execute('insert into user (id, name) values (%s, %s)', ('1', 'Michael'))
+cursor.execute('insert into user (id, name) values (%s, %s)', ('1', 'changhf'))
 print('rowcount =', cursor.rowcount)
 # 提交事务:
 conn.commit()
